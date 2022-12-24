f0ed61d5b5bb7f98289372f642a4f2581972d519
TRJackson998 <t.r.jackson998@gmail.com>
2022-12-23 17:33:14 -0800

    Forgot category_mappings.py in initial commit
    Issue: 6 - Analyze Finances

---
+++ Analyze_Finances/Assets/category_mappings.py
@@ -0,0 +1,23 @@
+category_mappings = {
+    # required categories for script
+    "Income": [],
+    "Internal Transfer": [],
+    # some suggested categories to get started
+    "Bills & Utilities": [],
+    "Car": [],
+    "Cash": [],
+    "Category Pending": [],
+    "Credit Card Payment": [],
+    "Education": [],
+    "Entertainment": [],
+    "External Transfer": [],
+    "Food & Dining": [],
+    "Gaming": [],
+    "Gas": [],
+    "Groceries": [],
+    "Home": [],
+    "Parking and Transportation": [],
+    "Personal Care": [],
+    "Pets": [],
+    "Shopping": [],
+}
