a93ec51b4952c5bf1abcb51791fe33b5b3a9e341
TRJackson998 <85368064+TRJackson998@users.noreply.github.com>
2025-06-21 08:20:36 -0700

    Landing page vs Dashboard

--- flaskapp/app/routes.py
+++ flaskapp/app/routes.py
@@ -10,6 +10,7 @@ from flask import Blueprint, g, redirect, render_template, url_for
 
 bp = Blueprint("routes", __name__)
 
+
 @bp.before_request
 def before_request():
     """Check if logged in first"""
@@ -17,12 +18,19 @@ def before_request():
         # if not logged in, redirect to login page
         return redirect(url_for("auth.login"))
 
+
 @bp.route("/")
 def landing():
-    """Renders landing page with today's date"""
+    """Renders landing page"""
+    return render_template("routes/landing.html")
+
+
+@bp.route("/dashboard")
+def dashboard():
+    """Renders dashboard page with today's date"""
     today_ = datetime.today()
-    date_string = f"{today_.strftime(r"%B, %d %Y")} at {today_.strftime(r"%I:%M%p")}"
+    date_string = f"{today_.strftime(r'%B, %d %Y')} at {today_.strftime(r'%I:%M%p')}"
     return render_template(
-        "routes/landing.html",
+        "routes/dashboard.html",
         current_datetime=date_string,
     )
